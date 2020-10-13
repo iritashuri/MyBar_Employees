a9c3928d90f8bd50e63fcaf44260488a9455a151
iritashuri <iritashuri5109@gmail.com>
2020-10-13 22:29:36 +0300

    Add employee registration method

--- app/src/main/java/com/example/mybaremplyees/Activity_Main.java
+++ app/src/main/java/com/example/mybaremplyees/Activity_Main.java
@@ -2,10 +2,14 @@ package com.example.mybaremplyees;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+
+import com.google.firebase.firestore.FirebaseFirestore;
 
 
 public class Activity_Main extends AppCompatActivity {
@@ -15,12 +19,29 @@ public class Activity_Main extends AppCompatActivity {
     private Button Main_BTN_updateDeals;
     private TextView Main_TXT_newEmployee;
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         findViews();
+
+        Main__BTN_addOrder.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Check if password exist
+
+                openUpdateDeals();
+            }
+        });
+
+        Main_TXT_newEmployee.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openRegisterEmployee();
+            }
+        });
     }
 
     private void findViews(){
@@ -28,5 +49,17 @@ public class Activity_Main extends AppCompatActivity {
         Main__BTN_addOrder = findViewById(R.id.Main__BTN_addOrder);
         Main_BTN_updateDeals = findViewById(R.id.Main_BTN_updateDeals);
         Main_TXT_newEmployee = findViewById(R.id.Main_TXT_newEmployee);
+    }
+
+    // Open updateDeals activity
+    private void openUpdateDeals(){
+        Intent intent = new Intent(Activity_Main.this, Activity_UpdateDeals.class);
+        startActivity(intent);
+    }
+
+    // Open RegisterEmployee activity
+    private void openRegisterEmployee(){
+        Intent intent = new Intent(Activity_Main.this, Activity_RegisterEmployee.class);
+        startActivity(intent);
     }
 }
--- app/src/main/java/com/example/mybaremplyees/Activity_RegisterEmployee.java
+++ app/src/main/java/com/example/mybaremplyees/Activity_RegisterEmployee.java
@@ -1,11 +1,24 @@
 package com.example.mybaremplyees;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class Activity_RegisterEmployee extends AppCompatActivity {
 
@@ -14,19 +27,120 @@ public class Activity_RegisterEmployee extends AppCompatActivity {
     private Button RegEmployee_BTN_Register;
     private ProgressBar RegEmployee_PRBR_progressBar;
 
+    private FirebaseFirestore db;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity__register_employee);
 
         findViews();
-    }
 
+        db = FirebaseFirestore.getInstance();
+
+        RegEmployee_BTN_Register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                RegisterEmployee();
+            }
+        });
+
+
+    }
 
     private void findViews(){
         RegEmployee_EDT_userName = findViewById(R.id.RegEmployee_EDT_userName);
         RegEmployee_EDT_password = findViewById(R.id.RegEmployee_EDT_password);
         RegEmployee_BTN_Register = findViewById(R.id.RegEmployee_BTN_Register);
         RegEmployee_PRBR_progressBar = findViewById(R.id.RegEmployee_PRBR_progressBar);
+    }
+    // Insert new employee details
+    private void RegisterEmployee(){
+        // Get data
+        final String user_name = RegEmployee_EDT_userName.getText().toString().trim();
+        final String password = RegEmployee_EDT_password.getText().toString().trim();
+
+        // Check if there are errors and return if there is
+        if(checkErrors(user_name, password)){
+            return;
+        }
+
+        // Show progress bar
+        RegEmployee_PRBR_progressBar.setVisibility(View.VISIBLE);
+
+        // Register the employee in firebase
+        insertDetails(user_name, password);
+
+    }
+
+    // Check if there are errors and return true if there is
+    boolean checkErrors(String user_name, String password){
+
+        // Check if there is no empty value
+        if(checkEmpties(user_name, password)){
+            return true;
+        }
+
+        // Validate password length
+        if(password.length() < 4){
+            RegEmployee_EDT_password.setError("Password Must include at least 4 characters. ");
+            return true;
+        }
+
+        return false;
+    }
+
+    // Check if email or password was not insert
+    private boolean checkEmpties(String user_name, String password){
+        boolean to_return = false;
+
+        if (checkEmptyTxt(user_name, RegEmployee_EDT_userName, "User name")){
+            to_return = true;
+        }
+        if (checkEmptyTxt(password, RegEmployee_EDT_password, "Password")){
+            to_return = true;
+        }
+        return to_return;
+    }
+
+
+    // Check if edit text is empty
+    private boolean checkEmptyTxt(String str, EditText editText, String missing){
+        if(TextUtils.isEmpty(str)){
+            editText.setError(missing + "is Required. ");
+            return true;
+        }
+        return false;
+    }
+
+    private void insertDetails(String user_name, String password){
+        // Create a new user with a first, middle, and last name
+        Map<String, Object> employee = new HashMap<>();
+        employee.put("user_name", user_name);
+        employee.put("password", password);
+
+        // Add a new document with a generated ID
+        db.collection("employees")
+                .add(employee)
+                .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                    @Override
+                    public void onSuccess(DocumentReference documentReference) {
+                        // Show success toast message
+                        Toast.makeText(Activity_RegisterEmployee.this, "Registered successfully", Toast.LENGTH_LONG).show();
+                        // Close activity
+                        finish();
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        // Show success toast message
+                        Toast.makeText(Activity_RegisterEmployee.this, "Error adding employee", Toast.LENGTH_LONG).show();
+                        Log.w("pttt", "Error adding document", e);
+                        // Stop showing progress bar
+                        RegEmployee_PRBR_progressBar.setVisibility(View.GONE);
+                    }
+                });
     }
 }
